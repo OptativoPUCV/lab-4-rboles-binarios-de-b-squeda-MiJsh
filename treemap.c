b1dfb456862cef89f3b31e2e2a89aaa0322e0a6f
Your Name <you@example.com>
2025-05-01 19:49:26 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -167,6 +167,28 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
 
 
 Pair * upperBound(TreeMap * tree, void* key) {
+    if (tree == NULL || tree -> root == NULL) return NULL;
+
+    TreeNode * current = tree -> root;
+    TreeNode * upper = NULL;
+    while (current != NULL) {
+        if (is_equal(tree, key, current -> pair -> key)) {
+            tree -> current = current;
+            return current -> pair;
+        }
+
+        if (tree ->lower_than(key, current -> pair -> key)) {
+            upper = current;
+            current = current -> left;
+        } else {
+            current = current -> right;
+        }
+    }
+
+    if (upper != NULL) {
+        tree -> current = upper;
+        return upper -> pair;
+    }
     return NULL;
 }
 
